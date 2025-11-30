04a826c9460022c0449d895477a9c5b9ed135eda
Abdul Rafay <abdulrafaycodeder@gmail.com>
2025-11-30 10:38:51 -0800

    MEW

--- index.js
+++ index.js
@@ -9,7 +9,34 @@
 // // console.log(a)
 
 
-var a = 1
-var b = 1
+// var a = 1
+// var b = 1
 
-var result = ++a + b++ + a++ + ++a + b++
+// var result = ++a + b++ + a++ + ++a + b++
+
+// console.log(result)
+
+
+
+
+
+// var number = 10
+
+// var newNumber = number + 1
+// console.log(newNumber)
+
+// var newNumber = number++; // post increment
+// var newNumber = ++number; // pre increment
+
+
+// console.log(newNumber)
+
+// console.log(number++)
+
+
+var a = 1 
+
+var test = a++
+
+console.log(a)
+
